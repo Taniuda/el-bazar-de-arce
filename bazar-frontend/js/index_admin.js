d700adc7c2b8d7f4371c01ff6cedf8ccd603a28b
Brayan Gonzalez <ortizbrayan872@gmail.com>
2024-11-12 17:26:58 -0600

    Modificacion index_admin.js Se modifico el index_admin.js para cectar con el api y mostrar los productos

--- bazar-frontend/js/index_admin.js
+++ bazar-frontend/js/index_admin.js
@@ -1,3 +1,28 @@
+//Conexion con el api
+document.addEventListener('DOMContentLoaded', async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        alert('Debes iniciar sesión para acceder a esta página.');
+        window.location.href = 'pages/login.html';
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:3001/api/products', {
+            headers: { 'Authorization': `Bearer ${token}` }
+        });
+
+        if (response.ok) {
+            const products = await response.json();
+            displayProducts(products);
+        } else {
+            console.error('Error al obtener los productos:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error al conectar con la API:', error);
+    }
+});
+
 // JavaScript para manejar las pestañas
 document.querySelectorAll('.tab').forEach(tab => {
     tab.addEventListener('click', () => {
@@ -11,8 +36,36 @@ document.querySelectorAll('.tab').forEach(tab => {
     });
 });
 
+//Mostrar los productos
+function displayProducts(products) {
+    const productList = document.getElementById('productList');
+    productList.innerHTML = ''; // Limpia la lista antes de agregar nuevos productos
+
+    products.forEach(product => {
+        const productItem = document.createElement('div');
+        productItem.className = 'product-item';
+
+        const productName = document.createElement('h3');
+        productName.textContent = product.nombre;
+        productItem.appendChild(productName);
+
+        const productPrice = document.createElement('span');
+        productPrice.textContent = `$${product.precio}`;
+        productItem.appendChild(productPrice);
+
+        // Añadir un enlace a la página de detalles del producto administrador
+        const productLink = document.createElement('a');
+        productLink.href = `pages/detalle-producto.html?id=${product.id}`;
+        productLink.textContent = 'Ver detalles';
+        productLink.className = 'product-link';
+        productItem.appendChild(productLink);
+
+        productList.appendChild(productItem);
+    });
+}
+
 function agregar_producto() { 
-    window.location.href = "../pages/agregar_producto.html";
+    window.location.href = "../pages/agregar_producto_admin.html";
 }
 
 function logout() {
