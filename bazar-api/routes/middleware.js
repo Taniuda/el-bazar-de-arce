9c4e5216e174423fb5adc32101ebb4f39cf25916
Brayan Gonzalez <ortizbrayan872@gmail.com>
2024-11-12 20:01:27 -0600

    Modificacion middleware.js Se modifico middleware.js para mejorar la autentificacion de los usuarios

--- bazar-api/routes/middleware.js
+++ bazar-api/routes/middleware.js
@@ -1,18 +1,27 @@
-// Middleware para verificar token y rol de administrador
+const jwt = require('jsonwebtoken');
+
+// Middleware para verificar token
 function verificarToken(req, res, next) {
     const token = req.headers['authorization'];
     if (!token) return res.status(403).json({ message: 'Token no proporcionado' });
-  
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return res.status(401).json({ message: 'Token inválido' });
-      req.user = decoded;
-      next();
+
+    jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ message: 'Token inválido' });
+        req.user = decoded;
+        next();
     });
-  }
-  
-  function verificarAdministrador(req, res, next) {
+}
+
+// Middleware para verificar rol de administrador
+function verificarAdministrador(req, res, next) {
     if (req.user.rol !== 'administrador') {
-      return res.status(403).json({ message: 'Acceso denegado: Solo para administradores' });
+        return res.status(403).json({ message: 'Acceso denegado: Solo para administradores' });
     }
     next();
-  }
+}
+
+// Exportar las funciones de middleware
+module.exports = {
+    verificarToken,
+    verificarAdministrador
+};
