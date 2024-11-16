aa51030aaf6a01c947abb867aca839e9c0aba7dc
Brayan Gonzalez <ortizbrayan872@gmail.com>
2024-11-15 21:35:53 -0600

    Modificacion detalle-producto.js Se modifico el archivo para agregar la funcionabilidad del boton comprar y agrego una variable para extraer el dato del precio del producto para usarlo en la peticion de compra

--- bazar-frontend/js/detalle-producto.js
+++ bazar-frontend/js/detalle-producto.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         return;
     }
 
+    let product;
+
     try {
         const response = await fetch(`http://localhost:3001/api/products/${productId}`, {
             headers: {
@@ -18,7 +20,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             throw new Error('Error al obtener los detalles del producto');
         }
 
-        const product = await response.json();
+        product = await response.json();
 
         document.getElementById('product-name').textContent = product.nombre;
         document.getElementById('product-image').src = `http://localhost:3001${product.imagen}`;
@@ -28,10 +30,45 @@ document.addEventListener('DOMContentLoaded', async () => {
         document.getElementById('product-category').textContent = product.clasificacion;
         document.getElementById('product-description').textContent = product.descripcion;
 
+        // Mostrar el mensaje si el estado es diferente a disponible 
+        if (product.estado !== 'disponible') { 
+            document.getElementById('mensaje-compra').style.display = 'block';
+        }
+
     } catch (error) {
         console.error(error);
         alert('Error al cargar los detalles del producto');
     }
+
+    const btnComprar = document.getElementById('btn-comprar');
+    btnComprar.addEventListener('click', async () => {
+        try {
+            const total = product.precio;
+
+            const response = await fetch('http://localhost:3001/api/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({ producto_id: productId, total })
+            });
+
+            if (response.ok) {
+                const resultado = await response.json();
+                alert('Compra realizada con éxito. ID del pedido: ' + resultado.pedido_id);
+                // Recargar la página para actualizar el estado del producto 
+                window.location.reload();
+            } else {
+                const errorData = await response.json();
+                alert('Error al realizar la compra: ' + errorData.error || response.statusText);
+            }
+        } catch (error) {
+            console.error('Error al realizar la compra:', error);
+            alert('Error al realizar la compra');
+        }
+    });
+
 });
 
 function regresar_index() { 
